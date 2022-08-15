58c0244b897712d3384b94bf2ae7091665a4fc04
Oriol Canalias <oriol.canalias@clearpay.com>
2022-08-15 21:53:47 +0200

    adding layered docker

--- free-example/src/main/kotlin/br/com/devcave/hexagonal/mybank/adapter/in/web/AccountController.kt
+++ free-example/src/main/kotlin/br/com/devcave/hexagonal/mybank/adapter/in/web/AccountController.kt
@@ -3,6 +3,7 @@ package br.com.devcave.hexagonal.mybank.adapter.`in`.web
 import br.com.devcave.hexagonal.mybank.adapter.`in`.web.request.CreateAccountRequest
 import br.com.devcave.hexagonal.mybank.applications.port.`in`.CreateAccountUseCase
 import br.com.devcave.hexagonal.mybank.common.Account
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,11 +15,15 @@ class AccountController(
     private val createAccountUseCase: CreateAccountUseCase
 ) {
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     @PostMapping
     fun createAccount(@RequestBody request: CreateAccountRequest): Account {
         return createAccountUseCase.createAccount(
             ownerId = request.ownerId,
             bankId = request.bankId
-        )
+        ).also {
+            logger.info("Response $it")
+        }
     }
 }
